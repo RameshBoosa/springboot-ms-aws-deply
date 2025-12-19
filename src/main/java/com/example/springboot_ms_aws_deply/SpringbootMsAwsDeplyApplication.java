aa781fa6fb67e50ee9588f671ac7ef057c039b89
Ramesh Boosa <rameshboosa.iosdev@gmail.com>
2025-12-18 16:57:04 -0800

    springboot-microservice first commit

---
+++ src/main/java/com/example/springboot_ms_aws_deply/SpringbootMsAwsDeplyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ms_aws_deply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsAwsDeplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsAwsDeplyApplication.class, args);
+	}
+
+}
