9a74dde42fff87334f89fc6d7692522c5df862d7
Ramesh Boosa <rameshboosa.iosdev@gmail.com>
2025-12-19 11:26:06 -0800

    compile error fixes

--- src/main/java/com/example/springboot_ms_aws_deply/RootController.java
+++ src/main/java/com/example/springboot_ms_aws_deply/RootController.java
@@ -1,5 +1,16 @@
 package com.example.springboot_ms_aws_deply;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/demo")
 public class RootController {
 
+    @GetMapping("/data")
+    public String getProducts() {
+        return "Welcome to Spring Boot Application Deployed on AWS!";
+    }
 }
